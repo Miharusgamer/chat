0f53c37563ebedbc54d65e48e85351529a6e4672
Konf3D <arturmakushev@gmail.com>
2022-12-07 14:27:45 +0700

    Added user and message classes

--- Project/source/message.cpp
+++ Project/source/message.cpp
@@ -1,2 +1,12 @@
 #pragma once
 #include "message.h"
+
+Message::Message(std::string& message_content, User& sender)
+	:m_content(message_content),m_sender(sender)
+{
+}
+
+PublicMessage::PublicMessage(std::string& message, User& sender)
+	:Message(message,sender)
+{
+}
--- Project/source/users.h
+++ Project/source/users.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <string>
+class User
+{
+public:
+	bool changePassword(std::string& currentPassword, std::string& newPassword);
+	bool changeUsername(std::string& currentPassword, std::string& newUsername);
+	bool login(std::string& password);
+	std::string getUsername() const;
+	std::string getLogin() const;
+
+public:
+	User() = delete;
+	User(std::string& login, std::string& username, std::string& password);
+	~User() = default;
+
+private:
+	const std::string& m_login;
+	std::string& m_password;
+	std::string& m_username;
+};
