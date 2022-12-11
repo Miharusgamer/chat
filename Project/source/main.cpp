ee931c9c0434aa377b07cacf680029e3ef7cfb36
Miha <ewqewqqwe1@gmail.com>
2022-12-11 17:48:42 +0300

    /

--- Project/source/main.cpp
+++ Project/source/main.cpp
@@ -1,8 +1,20 @@
+#include "Chat.h"
 #include <iostream>
-
-using namespace std;
 
 int main()
 {
+	Chat chat;
 
+	chat.start();
+
+	while (chat.isChatWork()) {
+
+		chat.showLoginMenu();
+
+		while (chat.getCurrentUser()) {
+			chat.showUserMenu();
+		}
+	}
+
+	return 0;
 }
